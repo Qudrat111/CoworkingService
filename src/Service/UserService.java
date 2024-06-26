7fd2b91c76a2b9b996f0ffb8b8d70874778fe81a
Qudrat111 <juratergashev580@gmail.com>
2024-06-26 14:39:58 +0500

    committed all things to Coworking_Service

--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -4,9 +4,23 @@ import Model.User;
 
 import java.util.HashMap;
 import java.util.Map;
+/**
+ * Service class for managing user registrations.
+ */
 
 public class UserService {
+    /**
+     * A map to store users by their username.
+     */
     private Map<String, User> users = new HashMap<>();
+    /**
+     * Registers a new user with the specified username, password, and admin status.
+     *
+     * @param username the username of the new user
+     * @param password the password of the new user
+     * @param isAdmin  whether the new user has admin privileges
+     * @return true if the user was successfully registered, false if the username is already taken
+     */
 
     public boolean registerUser(String username, String password, boolean isAdmin) {
         if(users.containsKey(username)) {
